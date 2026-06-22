662e4c73cbee5acc92eb00f7b9b5e123942e4b57
devjanj <95684493+devjanj@users.noreply.github.com>
2026-06-22 16:38:22 +0200

    feat: defaultripple update

---
+++ setup-account.js
@@ -0,0 +1,33 @@
+const xrpl = require('xrpl')
+
+const TESTNET_URL = 'wss://s.altnet.rippletest.net:51233'
+
+async function main() {
+  const client = new xrpl.Client(TESTNET_URL)
+  await client.connect()
+  console.log('Connected to XRPL Testnet')
+
+  const issuer = xrpl.Wallet.fromSeed('sEd7kFu36bDadFsvecpPFHmM9r2iJPd')
+  console.log('Issuer:', issuer.address)
+
+  const result = await client.submitAndWait(
+    {
+      TransactionType: 'AccountSet',
+      Account: issuer.address,
+      SetFlag: xrpl.AccountSetAsfFlags.asfDefaultRipple,
+    },
+    { wallet: issuer },
+  )
+
+  console.log('Result:', result.result.meta.TransactionResult)
+  console.log('Hash:  ', result.result.hash)
+  console.log('Tx URL:', `https://testnet.xrpl.org/transactions/${result.result.hash}`)
+  console.log('Account:', `https://testnet.xrpl.org/accounts/${issuer.address}`)
+
+  await client.disconnect()
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
